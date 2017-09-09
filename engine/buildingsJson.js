c25ea712c1e45eec40fb127a0d896296cec21d15
Zelig880 <scuomo1988@gmail.com>
2017-09-09 22:59:13 +0100

    Push basic buolding json that will then be moved into mongo

---
+++ engine/buildingsJson.js
@@ -0,0 +1,16 @@
+const buildings = [
+        { name: "Wood Cutter", Summary: "Provide raw wood necessary for building construction", details: 
+            [
+                { level:1,  wood: 10, stone: 5, Time: 120, production: 2 },
+                { level:2,  wood: 20, stone: 10, Time: 180, production: 5 }
+            ] 
+            
+        },
+        { name: "Stone Cutter", Summary: "Provide raw stone necessary for building construction", details: 
+            [
+                { level:1,  wood: 5, stone: 10, Time: 120, production: 2 },
+                { level:2,  wood: 10, stone: 20, Time: 180, production: 5 }
+            ] 
+        },
+];
+
