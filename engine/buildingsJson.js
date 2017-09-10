1500e9ca0ec9d700210f0d26ef7973d622851633
Zelig880 <scuomo1988@gmail.com>
2017-09-10 22:30:05 +0100

    rename the building to be the same as the assets name ( will change int he future)

--- engine/buildingsJson.js
+++ engine/buildingsJson.js
@@ -1,12 +1,25 @@
 const buildings = [
-        { name: "Wood Cutter", Summary: "Provide raw wood necessary for building construction", details: 
+        { name: "A", Summary: "Provide raw wood necessary for building construction", details: 
             [
                 { level:1,  wood: 10, stone: 5, Time: 120, production: 2 },
                 { level:2,  wood: 20, stone: 10, Time: 180, production: 5 }
             ] 
             
         },
-        { name: "Stone Cutter", Summary: "Provide raw stone necessary for building construction", details: 
+        { name: "B", Summary: "Provide raw stone necessary for building construction", details: 
+            [
+                { level:1,  wood: 5, stone: 10, Time: 120, production: 2 },
+                { level:2,  wood: 10, stone: 20, Time: 180, production: 5 }
+            ] 
+        },
+        { name: "C", Summary: "Provide raw wood necessary for building construction", details: 
+            [
+                { level:1,  wood: 10, stone: 5, Time: 120, production: 2 },
+                { level:2,  wood: 20, stone: 10, Time: 180, production: 5 }
+            ] 
+            
+        },
+        { name: "D", Summary: "Provide raw stone necessary for building construction", details: 
             [
                 { level:1,  wood: 5, stone: 10, Time: 120, production: 2 },
                 { level:2,  wood: 10, stone: 20, Time: 180, production: 5 }
